dc1481821c1ed36459d5a3d21214eb849356cf79
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2018-11-09 18:35:29 -0500

    Fixed static class

--- Main/src/models/Packet.java
+++ Main/src/models/Packet.java
@@ -1,24 +1,18 @@
 package models;
 
 public class Packet {
-
+  
   /**
    * Used in the constructor to specifiy the type (role)
    * of the packet that is sent. 
    */
-  public class Type {
-    // Information.
-    public static byte I = (byte) 'I';
-    // Connection Request.
-    public static byte C = (byte) 'C';
-    // Confirmation. 
-    public static byte A = (byte) 'A';
-    // Rejection.
-    public static byte R = (byte) 'R';
-    // End Communications.
-    public static byte F = (byte) 'F';
-    // P bits.
-    public static byte P = (byte) 'P';
+  public static enum Type {
+    INFORMATION,
+    CONNECTION_REQUEST,
+    ACCEPTED_CONNECTION_REQUEST,
+    REFECTED_PAQUET,
+    ENDING_CONNECTION,
+    P_BITS
   }
   
   // Used for the bits stuffing.
@@ -36,14 +30,14 @@ public class Packet {
 
   // Attributes
   private byte id;
-  private byte type;
+  private Type type;
   private String data;
 
   /**
    * @param id Identifies the packet (0-7).
    * @param type Identifies the type of the packet (see class Type).
    */
-  public Packet(byte id, byte type, String data) {
+  public Packet(byte id, Type type, String data) {
     this.id = id;
     this.type = type;
     this.data = data;
@@ -51,5 +45,22 @@ public class Packet {
 
   private Byte[] toBinary() {
     return new Byte[1];
+  }
+
+  private byte getTypeByte() {
+    switch (this.type) {
+      case Type.INFORMATION:
+        return (byte) 'I';
+      case Type.CONNECTION_REQUEST:
+        return (byte) 'C';
+      case Type.ACCEPTED_CONNECTION_REQUEST:
+        return (byte) 'A';
+      case Type.REFECTED_PAQUET:
+        return (byte) 'R';
+      case Type.ENDING_CONNECTION:
+        return (byte) 'F';
+      default:
+        return (byte) 'P';
+    }
   }
 }
