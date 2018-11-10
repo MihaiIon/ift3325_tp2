9f9712aabaf29f222ad773e8ac5e7d0a3e79970c
Tristan f <tristf7@gmail.com>
2018-11-09 19:52:35 -0500

    socket monitor

--- Main/src/sender/Sender.java
+++ Main/src/sender/Sender.java
@@ -8,16 +8,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Sender implements SocketMonitor.PacketReceptionListener {
 
-    String filepath;
-    int backN;
-    int position;
+    private String filepath;
+    private int backN;
+    private int position = 0;
 
-    SocketMonitor SocketMonitor;
-    Socket clientSocket;
-    OutputStream out;
+    private SocketMonitor SocketMonitor;
+    private Socket clientSocket;
+    private OutputStream out;
+
+    private ArrayList<Packet> unconfirmedPackets = new ArrayList<>();
 
     public Sender(String hostname, int port, String filePath, int backN) throws IOException {
         clientSocket = new Socket(hostname, port);
@@ -29,7 +32,11 @@ public class Sender implements SocketMonitor.PacketReceptionListener {
         this.backN = backN;
     }
 
-    //https://www.journaldev.com/709/java-read-file-line-by-line
+    /*
+     *   https://www.journaldev.com/709/java-read-file-line-by-line
+     *
+     *   Lit un fichier ligne par ligne et les envoie
+     */
     public void run() {
         BufferedReader reader;
         try {
@@ -37,8 +44,6 @@ public class Sender implements SocketMonitor.PacketReceptionListener {
             String line = reader.readLine();
             while (line != null) {
                 line = reader.readLine();
-
-
             }
             reader.close();
         } catch (IOException e) {
@@ -49,6 +54,25 @@ public class Sender implements SocketMonitor.PacketReceptionListener {
 
     @Override
     public void onPacketReceived(Packet packet) {
+        switch (packet.getType()) {
+            case PAQUET_RECEPTION: {
+
+                break;
+            }
+            case REFECTED_PAQUET: {
+
+                break;
+            }
+        }
+    }
+
+    private void sendData(String data) {
+        Packet packet = new Packet(new byte[1], Packet.Type.INFORMATION, data); //TODO byte[1] remplacer par num de trame
+        unconfirmedPackets.add(packet);
+        out.write(packet.toBinary());
+    }
+
+    private void canSend(int packetPosition) {
 
     }
 }
