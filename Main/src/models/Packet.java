9ce76ea32558642aeb66bdbb5dd04a2b3325e663
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2018-11-11 14:57:32 -0500

    Update

--- Main/src/models/Packet.java
+++ Main/src/models/Packet.java
@@ -1,45 +1,74 @@
 package models;
 
 public class Packet {
-  
+
+  // ------------------------------------------------------------------------
+  // Static
+
   /**
    * Used in the constructor to specifiy the type (role)
    * of the packet that is sent. 
    */
-  public static enum Type {
+  public enum Type {
     INFORMATION,
     CONNECTION_REQUEST,
-    PAQUET_RECEPTION,
-    REFECTED_PAQUET,
+    PACKET_RECEPTION,
+    REJECTED_PACKET,
     ENDING_CONNECTION,
     P_BITS
   }
   
   // Used for the bits stuffing.
   private static byte FLAG = (byte) 126;
-  
-  // ------------------------------------------------------------------------
-  // Static Methods
 
-  public static Packet convertToPacket(byte[] data) {
-    return new Packet(0, Type.INFORMATION, "");
+  /**
+   * Converts the provided binary data to a Packet Object.
+   * @param stream Stream of bites reprensenting the packet.
+   * @return Packet Object.
+   */
+  public static Packet convertToPacket(String stream) {
+    return new Packet((byte)0, Type.INFORMATION, "TODO");
   }
 
+  /**
+   * @param type The type of the packet.
+   * @return Encodes the type of the packet on 8 bits (byte).
+   */
   private static byte convertTypeToByte(Type type) {
     switch (type) {
-      case Type.INFORMATION:
+      case INFORMATION:
         return (byte) 'I';
-      case Type.CONNECTION_REQUEST:
+      case CONNECTION_REQUEST:
         return (byte) 'C';
-      case Type.PAQUET_RECEPTION:
+      case PACKET_RECEPTION:
         return (byte) 'A';
-      case Type.REFECTED_PAQUET:
+      case REJECTED_PACKET:
         return (byte) 'R';
-      case Type.ENDING_CONNECTION:
+      case ENDING_CONNECTION:
         return (byte) 'F';
       default:
         return (byte) 'P';
     }
+  }
+
+  /**
+   * @param b Byte to be converted to String.
+   * @return Provides a String of bits representing the byte.
+   */
+  private static String convertByteToString(byte b) {
+    return String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
+  }
+
+  /**
+   * @param bs Array of bytes.
+   * @return Provides a String of bits representing each byte.
+   */
+  private static String convertBytesToString(byte[] bs) {
+    String str = "";
+    for (int i = 0; i < bs.length; i++){
+      str += convertByteToString(bs[i]);
+    }
+    return str;
   }
 
   // ------------------------------------------------------------------------
@@ -60,31 +89,52 @@ public class Packet {
     this.data = data;
   }
 
-  private Byte[] toBinary() {
-    return new Byte[1];
+  /**
+   * Converts Packet object to binary number.
+   */
+  public String toBinary() {
+    byte[] bytes = new byte[4];
+    bytes[0] = FLAG;
+    bytes[1] = this.id;
+    bytes[2] = this.type;
+    bytes[3] = FLAG;
+    return convertBytesToString(bytes);
   }
 
+  // ------------------------------------------------------------------------
+  // Getters
+
+  /**
+   * @return Provides the identifier of the packet.
+   */
   public int getId() {
     return this.id;
   }
 
+  /**
+   * @return Provides the type of the packet.
+   */
   public Type getType() {
-    switch ((char) (this.type & 0xFF)) {
+    char type = (char) (this.type & 0xFF);
+    switch (type) {
       case 'I':
-        return Type.INFORMATION:
+        return Type.INFORMATION;
       case 'C':
-        return Type.CONNECTION_REQUEST:
+        return Type.CONNECTION_REQUEST;
       case 'A':
-        return Type.PAQUET_RECEPTION:
+        return Type.PACKET_RECEPTION;
       case 'R':
-        return Type.REFECTED_PAQUET:
+        return Type.REJECTED_PACKET;
       case 'F':
-        return Type.ENDING_CONNECTION:
+        return Type.ENDING_CONNECTION;
       default:
         return Type.P_BITS;
     }
   }
 
+  /**
+   * @return Provides the data contained in the packet.
+   */
   public String getData() {
     return this.data;
   }
