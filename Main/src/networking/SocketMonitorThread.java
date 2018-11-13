4100ad276ed66fbc003bc5aba2f8f2446d05b4e3
Tristan f <tristf7@gmail.com>
2018-11-13 10:19:45 -0500

    -ajouter le timeout monitor

--- Main/src/networking/SocketMonitorThread.java
+++ Main/src/networking/SocketMonitorThread.java
@@ -6,6 +6,7 @@ import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Un socket qui gere envoie et recoie des packets au lieu de bytes et qui ecoute la reception de packets
@@ -15,6 +16,8 @@ public class SocketMonitorThread extends Thread {
 
     private PacketReceptionListener receptionListener;
     private DataInputStream in;
+
+    private AtomicInteger packetsReceived = new AtomicInteger(0);
 
     public SocketMonitorThread(Socket socket, PacketReceptionListener receptionListener) throws IOException {
         in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
@@ -29,13 +32,44 @@ public class SocketMonitorThread extends Thread {
             String input;
             while ((input = in.readUTF()) != null) {
                 System.out.println(input);
+                if(receptionListener != null) {
+                    // TODO receptionListener.onPacketReceived();
+                }
+                new TimeOutMonitor(packetsReceived.incrementAndGet()).run();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public interface PacketReceptionListener{
+    /*
+     * Attends 3 secondes et averti que le timeout de 3 secondes a été atteint
+     */
+    private class TimeOutMonitor implements Runnable {
+
+        final int packetNumber;
+
+        TimeOutMonitor(final int packetNumber) {
+            this.packetNumber = packetNumber;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(3000);
+                if(packetsReceived.get() <= packetNumber) {
+                    receptionListener.onPacketReceptionTimeOut();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public interface PacketReceptionListener {
         void onPacketReceived(PacketModel packet);
+
+        void onPacketReceptionTimeOut();
     }
 }
